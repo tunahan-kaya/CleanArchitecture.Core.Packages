f00a504b28caf7f4300e63d6bebebc5882cf3851
Tunahan Kaya <77425061+tunahankaya16@users.noreply.github.com>
2024-02-10 07:50:41 +0300

    Global error management implementation.

---
+++ Core.CrossCuttingConcerns/Exceptions/Handlers/ExceptionHandler.cs
@@ -0,0 +1,23 @@
+﻿using Core.CrossCuttingConcerns.Exceptions.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Exceptions.Handlers;
+
+public abstract class ExceptionHandler
+{
+	public Task HandleExceptionAsync(Exception exception) =>
+		exception switch
+		{
+			BusinessException businessException => HandleException(businessException),
+			_=>HandleException(exception)
+		};
+    /* gelen exception BusinessException türündeyse businessException'ı Handle et.
+    * Başka türdeyse ("_") onu Handle et. */
+	protected abstract Task HandleException(BusinessException businessException);
+    protected abstract Task HandleException(Exception exception); 
+
+}
